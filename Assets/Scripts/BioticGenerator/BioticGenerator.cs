ab22c374a88a40b962a2479a3337ea0819d5ba17
Borna Cvitanić <borna.cvitanic@gmail.com>
2021-10-01 15:22:42 +0200

    Implemented Biotic Generator animation

--- Assets/Scripts/BioticGenerator/BioticGenerator.cs
+++ Assets/Scripts/BioticGenerator/BioticGenerator.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class BioticGenerator : MonoBehaviour, IInteract
 {
     [SerializeField] private GameObject bioticField;
+    [SerializeField] private GameObject flapController;
     [SerializeField] private int timeout = 10;
+    [SerializeField] private float flapExtensionTime = 0.25f;
     private bool active = false;
     private float timeOfTimeout = 0;
+    private Vector3 flapControllerDefaultPosition;
+    private Vector3 flapControllerCurrentPosition;
+    private Vector3 flapControllerTargetPosition;
+    private float flapChangeTime;
 
     public void Interact(GameObject origin)
     {
@@ -22,16 +28,25 @@ public class BioticGenerator : MonoBehaviour, IInteract
     {
         active = true;
         bioticField.SetActive(true);
+        flapControllerCurrentPosition = flapController.transform.localPosition;
+        flapControllerTargetPosition = flapControllerDefaultPosition - new Vector3(0, 0.77f, 0);
+        flapChangeTime = Time.time;
     }
 
     private void DisableField()
     {
         active = false;
         bioticField.SetActive(false);
+        flapControllerCurrentPosition = flapController.transform.localPosition;
+        flapControllerTargetPosition = flapControllerDefaultPosition;
+        flapChangeTime = Time.time;
     }
 
     private void Start()
     {
+        flapControllerDefaultPosition = flapController.transform.localPosition;
+        flapControllerCurrentPosition = flapController.transform.localPosition;
+        flapControllerTargetPosition = flapControllerDefaultPosition;
         DisableField();
     }
 
@@ -41,6 +56,10 @@ public class BioticGenerator : MonoBehaviour, IInteract
         if(Time.time > timeOfTimeout && active)
         {
             DisableField();
+        }
+        if((Time.time - flapChangeTime) / flapExtensionTime <= 1)
+        {
+            flapController.transform.localPosition = Vector3.Lerp(flapControllerCurrentPosition, flapControllerTargetPosition, (Time.time - flapChangeTime) / flapExtensionTime);
         }
     }
 }
